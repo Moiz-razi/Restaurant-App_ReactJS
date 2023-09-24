e56983d01f8795ab94ed13d367542f8ddc3eb171
Moiz <moizuddinr189@gmail.com>
2023-09-24 17:35:49 +0530

    Header of Reataurant App

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import {Fragment} from "react";
+import mealsImg from '../../assests/meals.jpg'
+import classes from './Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = props =>{
+    return(
+      <Fragment>
+        <header className={classes.header}>
+           <h2>Babylon Meals</h2>
+           <HeaderCartButton/>
+        </header>
+
+        <div className={classes['main-image']}>
+         <img src={mealsImg} alt="Table with delicious Foods"/>
+        </div>
+      </Fragment>
+    )
+}
+
+export default Header;
